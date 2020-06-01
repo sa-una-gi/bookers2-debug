601bee9ec9e456578b4ea595a932eed9cef0c06f
k-saunagi <k5.daha55@gmail.com>
2020-06-01 17:28:10 +0900

    books index,show,new OK

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,20 @@
 class BooksController < ApplicationController
+  before_action :authenticate_user!
 
   def show
   	@book = Book.find(params[:id])
+    @user = User.find_by(id: @book.user_id)
   end
 
   def index
   	@books = Book.all #一覧表示するためにBookモデルの情報を全てくださいのall
+    @book = Book.new
   end
 
   def create
   	@book = Book.new(book_params) #Bookモデルのテーブルを使用しているのでbookコントローラで保存する。
-  	if @book.save #入力されたデータをdbに保存する。
+    @book.user_id = current_user.id
+  	if @book.save(book_params) #入力されたデータをdbに保存する。
   		redirect_to @book, notice: "successfully created book!"#保存された場合の移動先を指定。
   	else
   		@books = Book.all
@@ -42,7 +46,7 @@ class BooksController < ApplicationController
   private
 
   def book_params
-  	params.require(:book).permit(:title)
+  	params.require(:book).permit(:title, :body, :user_id)
   end
 
 end
