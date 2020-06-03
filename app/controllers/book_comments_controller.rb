da919475b185ec8a15d0df8ec2ca7dc9c54e88e4
k-saunagi <k5.daha55@gmail.com>
2020-06-03 15:30:31 +0900

    非同期でコメント機能実装

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,19 +1,22 @@
 class BookCommentsController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    book_comment = current_user.book_comments.new(book_comment_params)
-    book_comment.book_id = book.id
-    book_comment.save
-    redirect_back(fallback_location: root_path)
+    @book = Book.find(params[:book_id])
+    @book_comments = BookComment.where(book_id: @book.id)
+    @book_comment = current_user.book_comments.new(book_comment_params)
+    @book_comment.book_id = @book.id
+    @book_comment.save
+    # redirect_back(fallback_location: root_path)
   end
 
   def destroy
-    book = Book.find(params[:book_id])
+    @book = Book.find(params[:book_id])
+    @comment = BookComment.find(params[:id])
+    @book_comments = BookComment.where(book_id: @book.id)
     BookComment.find_by(
                   id: params[:id],
                   book_id: params[:book_id]).destroy
     # book_comment.destroy
-    redirect_back(fallback_location: root_path)
+    # redirect_back(fallback_location: root_path)
   end
 
   private
