db8ad9c576e06e5aa2deb26d66dbc98332c71d21
k-saunagi <k5.daha55@gmail.com>
2020-06-03 12:19:07 +0900

    非同期でいいね機能実装

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,22 +1,20 @@
 class FavoritesController < ApplicationController
   def create
-    book = Book.find(params[:book_id])
-    favorite = Favorite.new(
+    @book = Book.find(params[:book_id])
+    @favorite = Favorite.new(
                 user_id: current_user.id,
-                book_id: book.id)
-    favorite.save
-    # redirect_to books_path
-    redirect_back(fallback_location: root_path)
+                book_id: @book.id)
+    @favorite.save
+    # redirect_back(fallback_location: root_path)
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = Favorite.find_by(
+    @book = Book.find(params[:book_id])
+    @favorite = Favorite.find_by(
                 user_id: current_user.id,
-                book_id: book.id)
-    favorite.destroy
-    # redirect_to books_path
-    redirect_back(fallback_location: root_path)
+                book_id: @book.id)
+    @favorite.destroy
+    # redirect_back(fallback_location: root_path)
   end
 
 end
